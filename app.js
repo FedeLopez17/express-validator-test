0fbd478bb8b60e2f4c5d986a3e3cb9533eb3d5fc
FedeLopez17 <fedelopez1712@gmail.com>
2024-10-06 20:19:13 -0300

    Feat - Implement 404 page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
+app.use((req, res) => res.status(404).render("404"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
